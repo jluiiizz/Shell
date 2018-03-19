2d57add4be437c7bc615295a216378ab9ca81cc2
John <joaoluiz.php@hotmail.com>
2018-03-19 02:07:22 +0000

    Added some necessary libraries in main.c

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
+// Including C libraries
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// Including linux libraries (API)
+#include <unistd.h>
+#include <limits.h>
+
+// Include our own libraries and files
+#include "utils.c"
+#include "shell.c"
 
 int main(int argc, char **argv)
 {
+    shell_loop();
 
     return 0;
 }
